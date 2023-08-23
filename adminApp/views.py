e4a787bb90f09499ce4fe2b42fec8d42a04dc6fb
AmalMeronzz <amalvc353@gmail.com>
2023-08-23 12:01:58 +0530

    first commit

---
+++ adminApp/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def hello(request):
+    return HttpResponse("hello")
+
+def admin_home(request):
+    return render(request,'admin/index.html')
+
